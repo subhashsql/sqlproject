8edfa8361264596c209de25d89cc60e823854127
subhashsql <139942267+subhashsql@users.noreply.github.com>
2023-08-26 12:16:43 +0530

    Add files via upload
    Here I have created a new table  and used cube and rollup operator

---
+++ Cube and Rollup operator.sql
@@ -0,0 +1,45 @@
+/* cube and Rollup operators
+created by -Subhash
+date-20-05-2023
+
+*/
+use School
+go
+
+CREATE TABLE dbo.cubeone
+(
+  Colour    VARCHAR(30) NOT NULL,
+  ItemName    VARCHAR(30) NOT NULL,
+  qty       INT        NOT NULL
+);
+INSERT INTO dbo.cubeone(Colour, ItemName, qty)
+VALUES
+  ('Black','Table',20),
+  ('Orange','chair',10),
+  ('Red','chair',10),
+  ('Yellow','chair',10),
+  ('Red','Table',30),
+  ('Yellow','Table',20),
+  ('Orange','Table',30),
+  ('Black','chair',15),
+  ('Yellow','chair',13),
+  ('Neon','chair',10)
+  -------------------------
+  select *
+  from dbo.cubeone
+  -------------------
+
+  /* using cube operator*/
+  
+  select ItemName,Colour,sum(qty) as Total
+  from dbo.cubeone
+  group by Itemname,Colour with cube
+  go
+
+  ---------------
+  --using rollup 
+  ----------
+    select ItemName,Colour,sum(qty) as Total
+  from dbo.cubeone
+  group by Itemname,Colour with rollup
+  go
