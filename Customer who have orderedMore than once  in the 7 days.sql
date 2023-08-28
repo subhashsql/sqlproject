e78f267fbf3159c6a6ef11c1cf5415c2814c64a1
subhashsql <139942267+subhashsql@users.noreply.github.com>
2023-08-28 12:08:53 +0530

    Add files via upload
    This query is to get the customer id who have placed order more than  once  in the last 7 days

---
+++ Customer who have orderedMore than once  in the 7 days.sql
@@ -0,0 +1,22 @@
+/*
+Query to find customerid who have done more than one transcation with in last 7 day
+
+by- subhash
+date-20-05-2023
+
+
+*/
+
+use northwind
+go
+
+select top 5 *
+from [dbo].[Orders]
+go
+
+select distinct h.CustomerID
+from [dbo].[Orders] h join [dbo].[Orders] b on
+		h.CustomerID = b.CustomerID
+	where h.OrderID <>  b.OrderID
+	and h.orderdate <=b.orderdate
+	and datediff (day,h.orderdate,b.orderdate) <8
