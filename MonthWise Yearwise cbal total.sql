1ca658c4c4c64cbfd2eba821b7eb3205eeffcf20
subhashsql <139942267+subhashsql@users.noreply.github.com>
2023-08-29 13:40:36 +0530

    Add files via upload

---
+++ MonthWise Yearwise cbal total.sql
@@ -0,0 +1,15 @@
+/*
+ to get balance month wise year wise
+ Query by- Subhash
+ 36-05-2023
+*/
+use tbank 
+go
+--------
+select  sum(cbal) as 'Total Balance',year(doo) as'Year',month(doo) as 'Month',Datename(month,Doo)
+from amaster
+group by year(doo),month(doo),Datename(month,Doo)
+go
+---------
+select datename(month,doo)
+from amaster
