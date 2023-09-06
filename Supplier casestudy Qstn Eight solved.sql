63452c7f432880f143778da6278b9df270be676e
Subhash <139942267+subhashsql@users.noreply.github.com>
2023-09-06 09:34:50 +0530

    Supplier casestudy Qstn Eight solved.sql

--- Supplier casestudy Qstn Eight solved.sql
+++ Supplier casestudy Qstn Eight solved.sql
@@ -4,7 +4,6 @@ BY:Subhash
 Date Modified - 13-04-23
 */
 ---8.List the names of the suppliers who have supplied exactly 10 units of part P1.
-
 	select name as 'suppliers who have supplied 10 pid 1'
 	from supplierMaster m  join 
 				(select pid,sid
